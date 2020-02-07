1b69144253ab53de1a016e0bf9dd2b198c1e9224
benibenj <bsimmond@student.ethz.ch>
2020-02-07 12:13:40 +0100

    blub

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("Hello World")
+import torch
+
+x = torch.Tensor([5, 3])
+y = torch.Tensor([2, 1])
+
+print(x*y)
+
+x = torch.zeros([2, 5])
+
+print(x)
+
+print(x.shape)
+
+y = torch.rand([2, 5])
+
+print(y)
